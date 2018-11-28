2457d9b0edff7215110e0e6ae51cbe2bbb344b11
Natnael Getahun <connect@ngetahun.me>
2018-11-28 10:36:55 +0100

    :recycle: Refactoring code
    - change file name
    - document functions

--- weak-heap/weak_heap.go
+++ weak-heap/weak_heap.go
@@ -1,5 +1,6 @@
 // Weak heap implementation
 // reference: https://en.wikipedia.org/wiki/Weak_heap
+// Implementation adopted from https://www.sanfoundry.com/cpp-program-implement-weak-heap/
 
 package weak_heap
 
@@ -9,18 +10,16 @@ import (
 
 // Weak heap data structure
 type WeakHeap struct {
-	root   *node
-	height int
+	root  *node
+	nodes []*node
+	// height int
 }
 
 type node struct {
-	item heap.Item
-
+	item     heap.Item
 	children []*node
-
-	parent *node
-
-	flag uint8
+	parent   *node
+	flag     uint8
 }
 
 func toggleFlag() {}
@@ -33,8 +32,30 @@ func (w *WeakHeap) init() *WeakHeap {
 	return w
 }
 
-func construct() *WeakHeap {
-	return nil
+// func construct() *WeakHeap {
+// 	return nil
+// }
+
+// Insert to weak heap structure
+func (w *WeakHeap) Insert(item heap.Item) heap.Item {
+	newnode := &node{item: item}
+	// w.root.children.append(newnode)
+	return newnode
+}
+
+// Delete the minimum node from weak heap structure
+func (w *WeakHeap) DeleteMin() heap.Item {}
+
+// Find the minimum node in the weak heap structure
+func (w *WeakHeap) FindMin() heap.Item {
+	// get a sorted heap
+	var temp *WeakHeap = sort(w, DESC)
+	return temp.root.item
+}
+
+// clears weak heap
+func (w *WeakHeap) Clear() {
+	w.root = &node{}
 }
 
 func (w *WeakHeap) Size() int {
@@ -47,18 +68,7 @@ func (w *WeakHeap) IsEmpty() bool {
 	return w.root.item == nil
 }
 
-// clears weak heap
-func (w *WeakHeap) Clear() {
-	w.root = &node{}
-}
-
 func (w *WeakHeap) merge() {}
-
-func (w *WeakHeap) FindMin() heap.Item {
-	// get a sorted heap
-	var temp *WeakHeap = sort(w, DESC)
-	return temp.root.item
-}
 
 // sorts the weak heap in an order given
 func sort(w *WeakHeap, order int) *WeakHeap {
@@ -66,13 +76,6 @@ func sort(w *WeakHeap, order int) *WeakHeap {
 }
 
 // Insert item to the WeakHeap structure
-func (w *WeakHeap) Insert(item heap.Item) heap.Item {
-	newnode := &node{item: item}
-	// w.root.children.append(newnode)
-	return newnode
-}
-
-func (w *WeakHeap) DeleteMin() heap.Item {}
 
 func (w *WeakHeap) join(i int, j int) {}
 
